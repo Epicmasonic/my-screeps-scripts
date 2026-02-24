289669c32a98522f96ac43d3435fa69c1aead0c8
Epicmasonic <epicmasonic@gmail.com>
2026-02-24 10:03:36 -0500

    Rework spawning to use a buildOrder

--- screeps.com/FunctionSpam/(Building actions) Spawn.js
+++ screeps.com/FunctionSpam/(Building actions) Spawn.js
@@ -1,37 +1,36 @@
-const maxHarvesters = 4;
+const harvestBody = [MOVE,MOVE,WORK,CARRY,MOVE]
+
+var buildOrder = ['Harvest','Harvest','Harvest','Harvest'];
 
 module.exports = {
 	/** @param {Creep} creep **/
 	run: function(spawn) {
-		let number = 1;
-		let harvesters = _.filter(Game.creeps, (creep) => creep.memory.role == 'Harvest');
-
-		/*
-		console.log();
-		console.log(harvesters);
-		console.log(harvesters.length);
-		console.log(!spawn.Spawning);
-		console.log(spawn.store[RESOURCE_ENERGY] >= 300);
-		console.log(harvesters.length < maxHarvesters);
-		console.log(!spawn.Spawning && spawn.store[RESOURCE_ENERGY] >= 300 && harvesters.length < maxHarvesters);
-		*/
-		
-		if (!spawn.spawning && spawn.store[RESOURCE_ENERGY] >= 300 && harvesters.length < maxHarvesters) {
-			let escape;
-			while (true) {
-				escape = true;
-				for (var name in Game.creeps) {
-					if (name == 'Harvest_' + number) {
-						escape = false;
-						number++;
-					}
-				}
-				if (escape) {
-					break;
-				}
-			}
+		if (!spawn.spawning && spawn.store[RESOURCE_ENERGY] >= 300) {
+			let harvestersMade = _.filter(Game.creeps, (creep) => creep.memory.role == 'Harvest').length;
+			let harvestersWanted = 0;
 			
-			spawn.spawnCreep([MOVE,MOVE,WORK,CARRY,MOVE], 'Harvest_' + number, {memory: {role: 'Harvest'}});
+			for (var role of buildOrder) {
+				switch (role) {
+					case 'Harvest':
+						harvestersWanted += 1;
+						if (harvestersMade < harvestersWanted) {
+							let escape = true;
+							let number = 1;
+							for (var name in Game.creeps) {
+								if (name == 'Harvest_' + number) {
+									escape = false;
+									number++;
+								}
+								if (escape) {
+									break;
+								}
+							}
+
+							spawn.spawnCreep(harvestBody, 'Harvest_' + number, {memory: {role: role}});
+							return;
+						}
+					}
+			}
 		}
 	}
 };
