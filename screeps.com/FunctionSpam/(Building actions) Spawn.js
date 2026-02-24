7bee00c31f1d6e07fbb4fc41d60cae56aa6b607d
Epicmasonic <epicmasonic@gmail.com>
2026-02-24 10:10:38 -0500

    Get extensions to work

--- screeps.com/FunctionSpam/(Building actions) Spawn.js
+++ screeps.com/FunctionSpam/(Building actions) Spawn.js
@@ -5,7 +5,20 @@ var buildOrder = ['Harvest','Harvest','Harvest','Harvest'];
 module.exports = {
 	/** @param {Creep} creep **/
 	run: function(spawn) {
-		if (!spawn.spawning && spawn.store[RESOURCE_ENERGY] >= 300) {
+		totalEnergyOwned = spawn.store[RESOURCE_ENERGY];
+		totalEnergyCapacity = spawn.store.getCapacity(RESOURCE_ENERGY);
+		for (var name of spawn.room.find(FIND_STRUCTURES, { filter: (structure) => structure.structureType == STRUCTURE_EXTENSION })) {
+			totalEnergyOwned += name.store[RESOURCE_ENERGY];
+			totalEnergyCapacity += name.store.getCapacity(RESOURCE_ENERGY);
+		}
+
+		spawn.room.visual.text(
+				totalEnergyOwned + '/' + totalEnergyCapacity,
+				spawn.pos.x, 
+				spawn.pos.y + 1.25, 
+				{opacity: 0.8});
+
+		if (!spawn.spawning && totalEnergyOwned >= 300) {
 			let harvestersMade = _.filter(Game.creeps, (creep) => creep.memory.role == 'Harvest').length;
 			let harvestersWanted = 0;
 			
